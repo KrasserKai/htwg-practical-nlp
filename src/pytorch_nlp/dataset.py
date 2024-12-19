62a92015831484896899517573e778459cd8e4f4
Kai Gaiser <gaiser.kai@gmail.com>
2024-12-19 18:24:41 +0100

    bit nicer

--- src/pytorch_nlp/dataset.py
+++ src/pytorch_nlp/dataset.py
@@ -3,7 +3,7 @@ from torch.utils.data import Dataset
 
 
 class SentimentDataset(Dataset):
-    def __init__(self, tweets, labels, vocab, processor, max_length=128):
+    def __init__(self, tweets, labels, processor, vocab=None, max_length=128):
         """
         Args:
             tweets (list of str): List of tweet strings.
@@ -15,10 +15,36 @@ class SentimentDataset(Dataset):
         self.labels = labels
         self.processor = processor
         self.max_length = max_length
-        self.vocab = vocab
+        self.vocab = self.build_vocab() if vocab is None else vocab
 
     def __len__(self):
         return len(self.tweets)
+
+    def build_vocab(self):
+        """
+        Dynamically build a vocabulary from the dataset.
+
+        Args:
+            tweets (list of str): List of tweet strings.
+
+        Returns:
+            dict: Token-to-index mapping.
+        """
+        from collections import Counter
+
+        # Tokenize all tweets and count token frequencies
+        counter = Counter()
+        for tweet in self.tweets:
+            tokens = self.processor(tweet)
+            counter.update(tokens)
+
+        # Assign a unique index to each token, starting from 2
+        # Reserve 0 for <PAD> and 1 for <UNK>
+        vocab = {"<PAD>": 0, "<UNK>": 1}
+        for token in counter:
+            vocab[token] = len(vocab)
+
+        return vocab
 
     def __getitem__(self, idx):
         tweet = self.tweets[idx]
